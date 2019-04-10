388649f3ad6be76be45b13914fe7b139d3b4253d
Talent Antonio <antonio.talenty@gmail.com>
2019-04-10 11:20:23 +0100

    changes

--- users/users-router.js
+++ users/users-router.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
+const checkRole = require('../auth/check-role-middleware')
+const Users = require('./users-model');
+const restricted = require('../auth/restricted-middleware');
 
-const Users = require('./users-model.js');
-const restricted = require('../auth/restricted-middleware.js');
-
-router.get('/', restricted, (req, res) => {
+router.get('/', restricted,  (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
